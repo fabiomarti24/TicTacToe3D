bacbd77120dbd65851ed0e56f7ef6e64feb649d7
fabio <fabio.marti@gmx.net>
2020-10-13 18:13:25 +0200

    Einleitung und Hardware

--- buch/chapters/10-arithmetik/kahan.c
+++
@@ -1,9 +0,0 @@
-double  s = 0;
-double  c = 0;
-for (int i = 1; i <= n; i++) {
-        double  y = a(i);
-        y = y - c;
-        double  t = s + y;
-        c = (t - s) - y;
-        s = t;
-}
--- buch/papers/qr/pics/solve.m
+++
@@ -1,3 +0,0 @@
-fun = @root;
-x0 = [1,1]
-x = fsolve(fun, x0);
